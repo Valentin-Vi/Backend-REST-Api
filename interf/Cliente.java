5e0f56ea26708b60ed35d0e39afb02180b2f439d
Valentin <153557240+Valentin-Vi@users.noreply.github.com>
2023-12-12 20:41:49 +0000

    Add files via upload

---
+++ interf/Cliente.java
@@ -0,0 +1,151 @@
+
+package soapws.interf;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for cliente complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="cliente"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="alquilerList" type="{http://interf.soapws/}alquiler" maxOccurs="unbounded" minOccurs="0"/&gt;
+ *         &lt;element name="dni" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *         &lt;element name="id_cliente" type="{http://www.w3.org/2001/XMLSchema}long" minOccurs="0"/&gt;
+ *         &lt;element name="nombre" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "cliente", propOrder = {
+    "alquilerList",
+    "dni",
+    "idCliente",
+    "nombre"
+})
+public class Cliente {
+
+    @XmlElement(nillable = true)
+    protected List<Alquiler> alquilerList;
+    protected String dni;
+    @XmlElement(name = "id_cliente")
+    protected Long idCliente;
+    protected String nombre;
+
+    /**
+     * Gets the value of the alquilerList property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the Jakarta XML Binding object.
+     * This is why there is not a <CODE>set</CODE> method for the alquilerList property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getAlquilerList().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link Alquiler }
+     * 
+     * 
+     */
+    public List<Alquiler> getAlquilerList() {
+        if (alquilerList == null) {
+            alquilerList = new ArrayList<Alquiler>();
+        }
+        return this.alquilerList;
+    }
+
+    /**
+     * Gets the value of the dni property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDni() {
+        return dni;
+    }
+
+    /**
+     * Sets the value of the dni property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDni(String value) {
+        this.dni = value;
+    }
+
+    /**
+     * Gets the value of the idCliente property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Long }
+     *     
+     */
+    public Long getIdCliente() {
+        return idCliente;
+    }
+
+    /**
+     * Sets the value of the idCliente property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Long }
+     *     
+     */
+    public void setIdCliente(Long value) {
+        this.idCliente = value;
+    }
+
+    /**
+     * Gets the value of the nombre property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Sets the value of the nombre property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNombre(String value) {
+        this.nombre = value;
+    }
+
+}
